8d482ec886b303d573930df25d1711e638958579
waleedhajalid10-coder <waleed.hajali.d10@gmail.com>
2025-09-04 03:03:49 +0300

    Update H3.py

--- H3.py
+++ H3.py
@@ -8,14 +8,3 @@ if num > 1:
          print(num, "check prime number:")
 else:
  print("The number must be greater than 1:")    
-# num = int(input("أدخل عدد: "))
-
-# if num > 1:  
-#     for i in range(2, num):  
-#         if num % i == 0:  
-#             print(num, "ليس عدداً أولياً")  
-#             break  
-#     else:  
-#         print(num, "عدد أولي")  
-# else:  
-#     print("العدد يجب أن يكون أكبر من 1")
